2e7983942fe4b97885845ad77ec658e80a0448a6
VexheslavSP <willy0@rambler.ru>
2023-04-04 17:06:37 +0300

    Что-нибудь

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import time
 import win32com.client
 import shutil
+import win32timezone
 
 
 def build_value_general(path_of_excell):
@@ -19,18 +20,19 @@ def build_value_general(path_of_excell):
     temp_503 = sheet.Cells(3, 2).value
     Hum_503 = sheet.Cells(5, 2).value
     Press_503 = sheet.Cells(7, 2).value
+    # как много протоколов и даты-отдельная переменная не входящая в класс эталон
     How_many_prot = sheet.Cells(7, 6).value
     date_num = sheet.Cells(2, 3).value
     dete_moun = sheet.Cells(1, 10).value
     value_general = [temp_et_arr, Hum_et_arr, temp_503,
                      Hum_503, How_many_prot, date_num, dete_moun, Press_503]
-    time.sleep(0.5)
+    time.sleep(0.1)
     wb.Save()
-    time.sleep(0.5)
+    time.sleep(0.1)
     wb.Close()
-    time.sleep(0.5)
+    time.sleep(0.1)
     Excel.Quit()
-    time.sleep(0.5)
+    time.sleep(0.1)
     return value_general
 
 
@@ -53,31 +55,44 @@ def build_instrumet(path_of_excell, count_instr):
     for i in range(5):
         Hum_mes_arr.append(sheet.Cells(ferst_row+6+i, 1+(count_instr*4)).value)
         i += 1
+    Model = "test"
     now_instrument = Instrument(
-        Number_instr, Number_ZK, Poveritel, temp_mes_arr, Hum_mes_arr)
-    time.sleep(0.5)
+        Number_instr, Number_ZK, Poveritel, temp_mes_arr, Hum_mes_arr, Model)
+    time.sleep(0.1)
     wb.Save()
-    time.sleep(0.5)
+    time.sleep(0.1)
     wb.Close()
-    time.sleep(0.5)
+    time.sleep(0.1)
     Excel.Quit()
-    time.sleep(0.5)
+    time.sleep(0.1)
     return now_instrument
 
 
 class Instrument:
     poz_ferst_cell = [12, 1]
 
-    def __init__(self, Number_instr, Number_ZK, Poveritel, temp_mes_arr, Hum_mes_arr):
+    def __init__(self, Number_instr, Number_ZK, Poveritel, temp_mes_arr, Hum_mes_arr, Model):
         self.Number_instr = Number_instr
         self.Number_ZK = Number_ZK
         self.Poveritel = Poveritel
         self.temp_mes_arr = temp_mes_arr
         self.Hum_mes_arr = Hum_mes_arr
+        self.Model = Model
 
 
-def build_prot(path_of_excell, Instrument, value_general):
+class Etalon:
+    def __init__(self, temp_et_arr, Hum_et_arr, temp_503, Hum_503, Press_503):
+        self.temp_et_arr = temp_et_arr
+        self.Hum_et_arr = Hum_et_arr
+        self.temp_503 = temp_503
+        self.Hum_503 = Hum_503
+        self.Press_503 = Press_503
+
+
+def build_prot(Instrument, value_general):
+    path_of_excell = Instrument.Path_sample
     Excel = win32com.client.Dispatch("Excel.Application")
+
     wb = Excel.Workbooks.Open(path_of_excell)
     sheet = wb.ActiveSheet
     sheet.Cells(7, 3).value = Instrument.Number_ZK
@@ -118,11 +133,11 @@ def build_prot(path_of_excell, Instrument, value_general):
         str(value_general[5].year)+'-'+temp_num_instr+'.xls'
     PathToSave = 'Y:/Пугачев_НЕ_УДАЛЯТЬ/503/'+NameFolder+'/'+NameProtok
     wb.Save()
-    time.sleep(0.5)
+    time.sleep(0.1)
     wb.Close()
-    time.sleep(0.5)
+    time.sleep(0.1)
     Excel.Quit()
-    time.sleep(0.5)
+    time.sleep(0.1)
 
     return PathToSave
 
@@ -133,6 +148,6 @@ general = build_value_general(generalExcellPath)
 i = 0
 while i < general[4]:
     tmp = build_instrumet(generalExcellPath, i)
-    tmpPath = build_prot(generalPathsample, tmp, general)
+    tmpPath = build_prot(tmp, general)
     shutil.copyfile(generalPathsample, tmpPath)
     i += 1
